d69be3b7c3ed63769e5ff53f3d1830877b8b65ba
kazurayam <kazuaki.urayama@gmail.com>
2023-10-07 06:57:31 +0900

    TC1

--- Keywords/my/JsonSchemaHelper.groovy
+++ Keywords/my/JsonSchemaHelper.groovy
@@ -1,12 +1,21 @@
 package my
 
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.networknt.schema.JsonSchema
 import com.networknt.schema.JsonSchemaFactory
 import com.networknt.schema.SpecVersion
 
 public class JsonSchemaHelper {
-	public static JsonSchema getJsonSchemaFromStringContent(String schemaContent) {
+	
+	private ObjectMapper mapper = new ObjectMapper()
+	
+	public JsonSchema getJsonSchemaFromStringContent(String schemaContent) {
 		JsonSchemaFactory factory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V4)
 		return factory.getSchema(schemaContent)
+	}
+	
+	public JsonNode getJsonNodeFromStringContent(String content) throws IOException {
+		return mapper.readTree(content);
 	}
 }
--- Scripts/TC1/Script1696627367341.groovy
+++ Scripts/TC1/Script1696627367341.groovy
@@ -1,7 +1,21 @@
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*
+
+import com.fasterxml.jackson.databind.JsonNode
 import com.networknt.schema.JsonSchema
+import com.networknt.schema.ValidationMessage
 
 import my.JsonSchemaHelper
 
-String schemaContent = "{\"enum\":[1, 2, 3, 4],\"enumErrorCode\":\"Not in the list\"}"
+String schemaContent = """
+{
+	\"enum\":[1, 2, 3, 4],
+	\"enumErrorCode\":\"Not in the list\"
+}
+"""
 
-JsonSchema schema = JsonSchemaHelper.getJsonSchemaFromStringContent(schemaContent)
+JsonSchemaHelper helper = new JsonSchemaHelper()
+JsonSchema schema = helper.getJsonSchemaFromStringContent(schemaContent)
+JsonNode node = helper.getJsonNodeFromStringContent("7")
+Set<ValidationMessage> errors = schema.validate(node)
+assertThat(errors.size(), is(1))
